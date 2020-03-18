6a1c44f90bc2e750b230783b625e26d219a78fb3
unknown <fedefava86@gmail.com>
2020-03-18 12:51:07 +0100

    [Add] Just to prove codepipeline

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 var cors = require('cors');
 
 var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 3000;
+  app = express(),
+  port = process.env.PORT || 3000;
 
 app.use(cors())
 
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.json([{
     "id": 1,
     "title": "Questo è un titolo"
+  },
+  {
+    "id": 2,
+    "title": "Questo è un altro cazzo di titolo"
   }])
 })
 
-app.listen(port, function(){
+app.listen(port, function () {
   console.log('Server listening on ', port);
 })
